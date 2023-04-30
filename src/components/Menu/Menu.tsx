e511038ec248b9882a43a543f90c520d131509af
Nicolas Cursino <nicolascursino@gmail.com>
2023-04-30 15:44:48 -0300

    estilizando componente menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Link } from "react-router-dom";
 function Menu() {
     return (
         <div style={{ padding: 10 }}>
-            <Link to="/lotofacil" style={{ marginLeft: 15, color:"#930089" }}>
+            <Link to="/lotofacil" style={{ marginLeft: 15, color:"#930089", fontWeight: 'bold', marginRight:'5px', fontSize: '20px', textDecorationLine: 'none' }}>
                 Lotofácil
             </Link>
-            <Link to="/megasena" style={{ marginLeft: 15, color:"#209869" }}>
+            <Link to="/megasena" style={{ marginLeft: 15, color:"#209869", fontWeight: 'bold', marginRight:'5px', fontSize: '20px', textDecorationLine: 'none'  }}>
                 Megasena
             </Link>
-            <Link to="/quina" style={{ marginLeft: 15, color:"#260085" }}>
+            <Link to="/quina" style={{ marginLeft: 15, color:"#260085", fontWeight: 'bold', marginRight:'5px', fontSize: '20px', textDecorationLine: 'none'  }}>
                 Quina
             </Link>
         </div>
